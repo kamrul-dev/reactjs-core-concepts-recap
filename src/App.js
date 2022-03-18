6fba635df0d27bf2fea9f536c90ec31044bd7ad2
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-18 07:45:28 +0600

    Component, styles, props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <District name="NoaKhali" speical="Bivag"></District>
+      <District name='Brahmonbaria' speical='Joddha Akbar'></District>
+      <District name='Cumilla' speical="Moyna moti"></District>
     </div>
   );
+}
+
+const districtStyle = {
+  backgroundColor: 'gray',
+  margin: '20ox',
+  borderRadius: '20px'
+
+}
+
+function District(props) {
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.speical}</p>
+    </div>
+  )
 }
 
 export default App;
